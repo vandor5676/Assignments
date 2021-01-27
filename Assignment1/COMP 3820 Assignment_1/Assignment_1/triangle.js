439231cdd66e69292456f9b58acb9e9bcfc95177
Shane <steiner5676@gmial.com>
2021-01-27 12:05:29 -0800

    bad s

--- Assignment1/COMP 3820 Assignment_1/Assignment_1/triangle.js
+++ Assignment1/COMP 3820 Assignment_1/Assignment_1/triangle.js
@@ -8,6 +8,7 @@ var program;
 var bufferId;
 var cBuffer;
 var squareTryangleCircle;
+var s = [];
 
 window.onload = function init() {
     var canvas = document.getElementById("gl-canvas");
@@ -46,42 +47,42 @@ window.onload = function init() {
     const numVerts = 100;
     var radius = 0.8
     circlePoints = [];
-    for(var i =0;i<numVerts;i++)
-    {
+    for (var i = 0; i < numVerts; i++) {
         var u = i / numVerts;
         var angle = u * 3.14159 * 2.0;
-        var pos = vec2(Math.cos(angle)* radius, Math.sin(angle)* radius) ;
+        var pos = vec2(Math.cos(angle) * radius, Math.sin(angle) * radius);
         circlePoints.push(pos);
     }
 
-    
+
 
     squareTryangleCircle = "circle";
-            var colors = [1, 0, 1, 1, 0, 1];
-            var numPoints = numVerts;
-            for (var i = 0; i < numPoints; i++)colors.push(1,0,1);
-    
+    var colors = [1, 0, 1, 1, 0, 1];
+    var numPoints = numVerts;
+    for (var i = 0; i < numPoints; i++)colors.push(1, 0, 1);
 
-            var bufferId = gl.createBuffer();
-            gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
-            gl.bufferData(gl.ARRAY_BUFFER, flatten(circlePoints), gl.STATIC_DRAW);
 
-            var aPosition = gl.getAttribLocation(program, "aPosition");
-            gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
-            gl.enableVertexAttribArray(aPosition);
+    var bufferId = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(circlePoints), gl.STATIC_DRAW);
 
-            cBuffer = gl.createBuffer();
-            gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
-            gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW); //new Float32Array(colors)
+    var aPosition = gl.getAttribLocation(program, "aPosition");
+    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(aPosition);
 
-            var aColor = gl.getAttribLocation(program, "aColor");
-            //3 points colors?
-            gl.vertexAttribPointer(aColor, 3, gl.FLOAT, false, 0, 0);
-            gl.enableVertexAttribArray(aColor);
-        
-            render("red");
+    cBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW); //new Float32Array(colors)
+
+    var aColor = gl.getAttribLocation(program, "aColor");
+    //3 points colors?
+    gl.vertexAttribPointer(aColor, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(aColor);
+
+    render("red");
 
     window.addEventListener('keydown', this.checkKey);
+    $("#initalButton").click(displayLetter)
 
 };
 
@@ -95,23 +96,89 @@ function render(colorName) {
     else if (colorName == "green") colorArray = [0, 1, 0];
     else if (colorName == "blue") colorArray = [0, 0, 1];
     else if (colorName == "favourite") colorArray = [.7, .1, 1];
+    else if (colorName == "random") colorArray = [Math.random(), Math.random(), Math.random()];
 
     if (squareTryangleCircle == "square") {
         numPoints = 4;
-        for (var i = 0; i < numPoints; i++ )colors.push(...colorArray);
+        for (var i = 0; i < numPoints; i++)colors.push(...colorArray);
     }
     else if (squareTryangleCircle == "trinagle") {
         numPoints = 3;
-        for (var i = 0; i < numPoints; i++ )colors.push(...colorArray);
+        for (var i = 0; i < numPoints; i++)colors.push(...colorArray);
     }
     else if (squareTryangleCircle == "circle") {
         numPoints = 100;
         for (var i = 0; i < numPoints; i++)colors.push(...colorArray);
     }
+    else if (squareTryangleCircle == "letter") {
+        numPoints = 240;
+        for (var i = 0; i < numPoints; i++)colors.push(...colorArray);
+       
+        gl.bufferSubData(gl.ARRAY_BUFFER, points, new Float32Array(colors));
+        gl.clear(gl.COLOR_BUFFER_BIT);
+        gl.drawArrays(gl.LINE_STRIP, 0, numPoints);
+        return;
+    }
+
 
     gl.bufferSubData(gl.ARRAY_BUFFER, points, new Float32Array(colors));
     gl.clear(gl.COLOR_BUFFER_BIT);
     gl.drawArrays(gl.TRIANGLE_FAN, 0, numPoints);
+}
+
+//displays the letter s 
+function displayLetter() {
+    var xypoints = [0.20, 0.84, 0.22, 0.84, 0.22, 0.84, 0.22, 0.84, 0.22, 0.84, 0.22, 0.84, 0.22, 0.84, 0.22, 0.84, 0.22, 0.84, 0.21, 0.84, 0.21, 0.84, 0.21, 0.85, 0.21, 0.85, 0.21, 0.85, 0.21, 0.85, 0.21, 0.85, 0.20, 0.85, 0.20, 0.85, 0.20, 0.85, 0.20, 0.85, 0.20, 0.85, 0.20, 0.85, 0.20, 0.85, 0.19, 0.85, 0.19, 0.85, 0.19, 0.85, 0.19, 0.85, 0.19, 0.85, 0.19, 0.85, 0.19, 0.85, 0.19, 0.85, 0.19, 0.85, 0.18, 0.85, 0.18, 0.85, 0.18, 0.85, 0.18, 0.86, 0.18, 0.86, 0.18, 0.86, 0.18, 0.86, 0.18, 0.86, 0.17, 0.86, 0.17, 0.86, 0.17, 0.86, 0.17, 0.86, 0.17, 0.86, 0.17, 0.86, 0.17, 0.86, 0.17, 0.86, 0.16, 0.86, 0.16, 0.86, 0.16, 0.86, 0.16, 0.86, 0.16, 0.86, 0.16, 0.86, 0.15, 0.86, 0.15, 0.86, 0.15, 0.87, 0.15, 0.87, 0.15, 0.87, 0.15, 0.87, 0.15, 0.87, 0.15, 0.87, 0.15, 0.87, 0.15, 0.87, 0.15, 0.87, 0.15, 0.87, 0.14, 0.87, 0.14, 0.87, 0.14, 0.87, 0.14, 0.87, 0.14, 0.88, 0.14, 0.88, 0.14, 0.88, 0.14, 0.88, 0.14, 0.88, 0.13, 0.88, 0.13, 0.88, 0.13, 0.88, 0.13, 0.89, 0.13, 0.89, 0.13, 0.89, 0.13, 0.89, 0.13, 0.89, 0.13, 0.89, 0.13, 0.89, 0.13, 0.89, 0.13, 0.89, 0.13, 0.90, 0.13, 0.90, 0.13, 0.90, 0.13, 0.90, 0.13, 0.90, 0.13, 0.90, 0.13, 0.91, 0.13, 0.91, 0.13, 0.91, 0.13, 0.91, 0.13, 0.91, 0.13, 0.91, 0.14, 0.91, 0.14, 0.91, 0.14, 0.91, 0.14, 0.91, 0.14, 0.91, 0.14, 0.91, 0.14, 0.91, 0.15, 0.91, 0.15, 0.92, 0.15, 0.92, 0.15, 0.92, 0.15, 0.92, 0.15, 0.92, 0.15, 0.92, 0.15, 0.92, 0.15, 0.92, 0.15, 0.92, 0.16, 0.92, 0.16, 0.92, 0.16, 0.92, 0.16, 0.92, 0.16, 0.92, 0.16, 0.92, 0.16, 0.92, 0.16, 0.92, 0.17, 0.92, 0.17, 0.92, 0.17, 0.92, 0.17, 0.92, 0.17, 0.92, 0.17, 0.92, 0.18, 0.92, 0.18, 0.92, 0.18, 0.92, 0.18, 0.92, 0.18, 0.92, 0.18, 0.92, 0.19, 0.92, 0.19, 0.92, 0.19, 0.92, 0.19, 0.93, 0.19, 0.93, 0.19, 0.93, 0.19, 0.93, 0.19, 0.93, 0.19, 0.93, 0.20, 0.93, 0.20, 0.93, 0.20, 0.93, 0.20, 0.93, 0.20, 0.93, 0.20, 0.93, 0.20, 0.93, 0.20, 0.93, 0.21, 0.93, 0.21, 0.93, 0.21, 0.93, 0.21, 0.94, 0.21, 0.94, 0.21, 0.94, 0.21, 0.94, 0.21, 0.94, 0.21, 0.94, 0.21, 0.94, 0.21, 0.94, 0.21, 0.94, 0.21, 0.94, 0.21, 0.95, 0.22, 0.95, 0.22, 0.95, 0.22, 0.95, 0.22, 0.95, 0.22, 0.95, 0.22, 0.95, 0.22, 0.96, 0.22, 0.96, 0.22, 0.96, 0.22, 0.96, 0.21, 0.96, 0.21, 0.96, 0.21, 0.96, 0.21, 0.96, 0.21, 0.96, 0.21, 0.97, 0.21, 0.97, 0.21, 0.97, 0.21, 0.97, 0.20, 0.97, 0.20, 0.97, 0.20, 0.97, 0.20, 0.97, 0.20, 0.97, 0.20, 0.97, 0.20, 0.97, 0.20, 0.97, 0.20, 0.97, 0.19, 0.97, 0.19, 0.98, 0.19, 0.98, 0.19, 0.98, 0.19, 0.98, 0.19, 0.98, 0.19, 0.98, 0.19, 0.98, 0.18, 0.98, 0.18, 0.98, 0.18, 0.98, 0.18, 0.98, 0.18, 0.98, 0.18, 0.98, 0.18, 0.98, 0.17, 0.98, 0.17, 0.98, 0.17, 0.98, 0.17, 0.98, 0.17, 0.98, 0.17, 0.98, 0.17, 0.98, 0.16, 0.98, 0.16, 0.98, 0.16, 0.98, 0.16, 0.98, 0.16, 0.98, 0.16, 0.98, 0.16, 0.98, 0.15, 0.98, 0.15, 0.98, 0.15, 0.98, 0.15, 0.98, 0.15, 0.98, 0.15, 0.98, 0.15, 0.98, 0.14, 0.98, 0.14, 0.98, 0.14, 0.98, 0.14, 0.98, 0.14, 0.98, 0.14, 0.98, 0.13, 0.98, 0.13, 0.99, 0.13, 0.99, 0.13, 0.99, 0.13, 0.99, 0.13, 0.99, 0.13, 0.99, 0.13, 1.00, 0.13, 1.00, 0.13, 1.00, 0.13, 1.00]
+    var x;
+    var even = true;
+    xypoints = $.map(xypoints, function (v) {
+
+        x = (v * 1.6) - 1
+        if (even) {
+            x = (x * -1) - .8
+            even = false;
+        }
+        else {
+            x = x - .5
+            even = true;
+        }
+
+        return ((x * 4) + .3);
+    });
+    var numbers = new Float32Array(xypoints);
+    squareTryangleCircle = "letter";
+    var colors = [1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0];
+
+    var bufferId = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
+    gl.bufferData(gl.ARRAY_BUFFER, numbers, gl.STATIC_DRAW);
+
+    var aPosition = gl.getAttribLocation(program, "aPosition");
+    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(aPosition);
+
+    cBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW); //new Float32Array(colors)
+
+    var aColor = gl.getAttribLocation(program, "aColor");
+    //3 points colors?
+    gl.vertexAttribPointer(aColor, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(aColor);
+
+    render("red");
+    // gl.clear(gl.COLOR_BUFFER_BIT);
+    // gl.drawArrays(gl.LINE_STRIP, 0, 240);
+
+}
+
+function myFunction(e) {
+    var x = e.clientX;
+    var y = e.clientY;
+    var coor = "Coordinates: (" + x + "," + y + ")";
+    document.getElementById("demo").innerHTML = coor;
+    s.push(x);
+    s.push(y);
 }
 
 //function that handles key presses
@@ -120,33 +187,23 @@ function checkKey(e) {
     switch (e.keyCode) {
         //red
         case 49:
-            var colors = [1, 0, 0, 1, 0, 0, 1, 0, 0];
-            gl.bufferSubData(gl.ARRAY_BUFFER, points, new Float32Array(colors));
             render("red");
             break;
         //green
         case 50:
-           // var colors = [0, 1, 0, 0, 1, 0, 0, 1, 0];
-           // gl.bufferSubData(gl.ARRAY_BUFFER, points, new Float32Array(colors));
             render("green");
             break;
         //blue
         case 51:
-            var colors = [0, 0, 1, 0, 0, 1, 0, 0, 1];
-            gl.bufferSubData(gl.ARRAY_BUFFER, pointsSquare, new Float32Array(colors));
             render("blue");
             break;
         //random color
         case 52:
-            var colors = [1, 0, 1, 0, 0, 0, 1, 0, 0];
-            gl.bufferSubData(gl.ARRAY_BUFFER, points, new Float32Array(colors));
-            renderTriangle();
+            render("random");
             break;
         //favourite color
         case 53:
-            var colors = [1, 0, 0, 1, 0, 0, 1, 0, 0];
-            gl.bufferSubData(gl.ARRAY_BUFFER, points, new Float32Array(colors));
-            renderTriangle();
+            render("favourite");
             break;
         //triangle
         case 84:
@@ -200,8 +257,8 @@ function checkKey(e) {
             squareTryangleCircle = "circle";
             var colors = [1, 0, 1, 1, 0, 1];
             var numPoints = 100;
-            for (var i = 0; i < numPoints; i++)colors.push(1,0,1);
-    
+            for (var i = 0; i < numPoints; i++)colors.push(1, 0, 1);
+
 
             var bufferId = gl.createBuffer();
             gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
@@ -219,7 +276,7 @@ function checkKey(e) {
             //3 points colors?
             gl.vertexAttribPointer(aColor, 3, gl.FLOAT, false, 0, 0);
             gl.enableVertexAttribArray(aColor);
-        
+
             render("favourite");
             break;
     }
