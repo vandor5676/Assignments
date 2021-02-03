d433e0163331d5d65f76042e616477813ccf9262
Shane <steiner5676@gmial.com>
2021-02-03 15:24:42 -0800

    back and forth with flip

--- Assignment2/assignment2.js
+++ Assignment2/assignment2.js
@@ -2,8 +2,9 @@
 
 var gl;
 var points ,points2;
-var LRTranslation;
-var backAndForth = 0.0;
+var LRTranslation, matrixLocation ;
+var backAndForth = 0.0; 
+var matrix =[-1,0,0,0,1,0,0,0,1];
 var angle =0;
 var program;
 
@@ -57,7 +58,12 @@ window.onload = function init()
     gl.vertexAttribPointer( aPosition, 2, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( aPosition );
 
+    //lookups
     LRTranslation = gl.getUniformLocation(program, "uLRTranslation");
+    matrixLocation = gl.getUniformLocation(program, "u_matrix");
+
+    // Set the matrix.
+   // gl.uniformMatrix3fv(matrixLocation, false, matrix);
 
     $("#startButton").click(start);
     $("#stopButton").click(stop);
@@ -84,11 +90,13 @@ function render() {
 
     //make shape go back and forth,  
     backAndForth = (Math.cos(angle)/1.43);
+
+    matrix = angle >=1*3.14159 ?[1,0,Math.cos(angle)/1.43,0,1,0,0,0,1]:[-1,0,Math.cos(angle)/1.43,0,1,0,0,0,1]
     angle = angle >=2*3.14159 ?0:angle+=0.05;
 
-   // square();
-
-    gl.uniform1f(LRTranslation, backAndForth);
+    //update parameters
+    gl.uniform1f(LRTranslation, backAndForth=0);
+    gl.uniformMatrix3fv(matrixLocation, false, matrix);
     
     gl.drawArrays( gl.TRIANGLE_STRIP, 0, 4 );
 
@@ -98,43 +106,13 @@ function render() {
     );
 }
 
-function flip()
-{
-    var bufferId = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
-    gl.bufferData( gl.ARRAY_BUFFER, points2, gl.STATIC_DRAW );
-}
+// var matrix = {
+//     flip: function()
+//     {
+//        return  [1,0,0,
+//         0,1,0,
+//     0,0,1]
+//     }
+// };
 
-function square_Binding(){
-    gl.useProgram( program2 );
-    gl.enableVertexAttribArray( s_vPosition );
-    gl.bindBuffer( gl.ARRAY_BUFFER, sBuffer );
-    gl.vertexAttribPointer( s_vPosition, 2, gl.FLOAT, false, 0, 0 );
-    gl.bindBuffer(gl.ARRAY_BUFFER, s_cBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colorS), gl.STATIC_DRAW );
-    s_ColorLoc = gl.getAttribLocation( program2, "aColor");
-    gl.vertexAttribPointer(s_ColorLoc, 3, gl.FLOAT, false, 0, 0);
-}
-function square()
-{
-   // squareTryangleCircle = "square";
-    var colors = [0, 1, 1, 0, 1, 1, 0.5, 0, 1, .6, .1, 1];
 
-    var bufferId = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
-    gl.bufferData(gl.ARRAY_BUFFER, points2, gl.STATIC_DRAW);
-
-    var aPosition = gl.getAttribLocation(program, "aPosition");
-    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(aPosition);
-
-    // cBuffer = gl.createBuffer();
-    // gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
-    // gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW); //new Float32Array(colors)
-
-    // var aColor = gl.getAttribLocation(program, "aColor");
-    // gl.vertexAttribPointer(aColor, 3, gl.FLOAT, false, 0, 0);
-    // gl.enableVertexAttribArray(aColor);
-
-    render();
-}
