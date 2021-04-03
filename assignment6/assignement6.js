6a81a1903f203e3ae3b9e629432a947ea933f6ab
Shane <steiner5676@gmail.com>
2021-04-03 16:24:11 -0700

    lighting

--- assignment6/assignement6.js
+++ assignment6/assignement6.js
@@ -4,37 +4,18 @@ var canvas;
 var gl;
 var program;
 
-var modelViewMatrix,projectionMatrix;
+var modelViewMatrix, projectionMatrix;
 
 var numPositions = 72;
 var normalsArray = [];
 // var numPositions = 36;
 
 var texSize = 64;
-
+var time =0;
 var flag = false;
+var perspectiveFlag = false;
 
-// Create a checkerboard pattern using floats
-
-
-// var image1 = new Array()
-// for (var i = 0; i < texSize; i++)  image1[i] = new Array();
-// for (var i = 0; i < texSize; i++)
-//     for (var j = 0; j < texSize; j++)
-//         image1[i][j] = new Float32Array(4);
-// for (var i = 0; i < texSize; i++) for (var j = 0; j < texSize; j++) {
-//     var c = (((i & 0x8) == 0) ^ ((j & 0x8) == 0));
-//     image1[i][j] = [c, c, c, 1];
-// }
-
-// Convert floats to ubytes for texture
-
-// var image2 = new Uint8Array(4 * texSize * texSize);
-
-// for (var i = 0; i < texSize; i++)
-//     for (var j = 0; j < texSize; j++)
-//         for (var k = 0; k < 4; k++)
-//             image2[4 * texSize * i + 4 * j + k] = 255 * image1[i][j][k];
+var lightPosition = vec4(0.0, 2.0, 0.0, 1.0);
 
 var positionsArray = [];
 var colorsArray = [];
@@ -88,14 +69,14 @@ var vertices = [
     vec4(0.5, 0.5, -0.5, 1.0),
     vec4(0.5, -0.5, -0.5, 1.0),
 
-    vec4(-0.5+2.5, -0.5+2.5, 0.5+2.5, 1.0),
-    vec4(-0.5+2.5, 0.5+2.5, 0.5+2.5, 1.0),
-    vec4(0.5+2.5, 0.5+2.5, 0.5+2.5, 1.0),
-    vec4(0.5+2.5, -0.5+2.5, 0.5+2.5, 1.0),
-    vec4(-0.5+2.5, -0.5+2.5, -0.5+2.5, 1.0),
-    vec4(-0.5+2.5, 0.5+2.5, -0.5+2.5, 1.0),
-    vec4(0.5+2.5, 0.5+2.5, -0.5+2.5, 1.0),
-    vec4(0.5+2.5, -0.5+2.5, -0.5+2.5, 1.0),
+    vec4(-0.5 + 2.5, -0.5 + 2.5, 0.5 + 2.5, 1.0),
+    vec4(-0.5 + 2.5, 0.5 + 2.5, 0.5 + 2.5, 1.0),
+    vec4(0.5 + 2.5, 0.5 + 2.5, 0.5 + 2.5, 1.0),
+    vec4(0.5 + 2.5, -0.5 + 2.5, 0.5 + 2.5, 1.0),
+    vec4(-0.5 + 2.5, -0.5 + 2.5, -0.5 + 2.5, 1.0),
+    vec4(-0.5 + 2.5, 0.5 + 2.5, -0.5 + 2.5, 1.0),
+    vec4(0.5 + 2.5, 0.5 + 2.5, -0.5 + 2.5, 1.0),
+    vec4(0.5 + 2.5, -0.5 + 2.5, -0.5 + 2.5, 1.0),
 ];
 
 
@@ -126,7 +107,6 @@ var yAxis = 1;
 var zAxis = 2;
 var axis = xAxis;
 
-//  var theta = vec3(45.0, 45.0, 45.0);
 var theta = vec3(0, 0, 0);
 
 var thetaLoc;
@@ -139,17 +119,13 @@ function configureTexture() {
     image.addEventListener('load', function () {
         gl.activeTexture(gl.TEXTURE0);
         gl.bindTexture(gl.TEXTURE_2D, texture);
-        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,gl.UNSIGNED_BYTE, image);
-
-       // gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, texSize, texSize, 0, gl.RGBA, gl.UNSIGNED_BYTE, image);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
         gl.generateMipmap(gl.TEXTURE_2D);
-        //gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST_MIPMAP_LINEAR);
-        //gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
     });
 
 
 }
-
+//loads arrays
 function quad(a, b, c, d, texCoord) {
 
     var t1 = subtract(vertices[b], vertices[a]);
@@ -190,19 +166,19 @@ function quad(a, b, c, d, texCoord) {
 
 
 function colorCube() {
-    quad(2, 3, 7, 6,texCoordLeft);
-    quad(1, 0, 3, 2,texCoordFront);
-    quad(3, 0, 4, 7,texCoordBottom);
-    quad(6, 5, 1, 2,texCoordTop);
-    quad(4, 5, 6, 7,texCoordBack);
-    quad(5, 4, 0, 1,texCoordRight);
+    quad(2, 3, 7, 6, texCoordLeft);
+    quad(1, 0, 3, 2, texCoordFront);
+    quad(3, 0, 4, 7, texCoordBottom);
+    quad(6, 5, 1, 2, texCoordTop);
+    quad(4, 5, 6, 7, texCoordBack);
+    quad(5, 4, 0, 1, texCoordRight);
 
-    quad(10, 11, 15, 14,texCoordLeft);
-    quad(9, 8, 11, 10,texCoordFront);
-    quad(11, 8, 12, 15,texCoordBottom);
-    quad(14, 13, 9, 10,texCoordTop);
-    quad(12, 13, 14, 15,texCoordBack);
-    quad(13, 12, 8, 9,texCoordRight);
+    quad(10, 11, 15, 14, texCoordLeft);
+    quad(9, 8, 11, 10, texCoordFront);
+    quad(11, 8, 12, 15, texCoordBottom);
+    quad(14, 13, 9, 10, texCoordTop);
+    quad(12, 13, 14, 15, texCoordBack);
+    quad(13, 12, 8, 9, texCoordRight);
 }
 
 
@@ -226,8 +202,12 @@ function init() {
     colorCube();
 
     var nBuffer = gl.createBuffer();
-      gl.bindBuffer(gl.ARRAY_BUFFER, nBuffer);
-      gl.bufferData(gl.ARRAY_BUFFER, flatten(normalsArray), gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, nBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(normalsArray), gl.STATIC_DRAW);
+
+    var normalLoc = gl.getAttribLocation(program, "aNormal");
+    gl.vertexAttribPointer(normalLoc, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(normalLoc);
 
     var cBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
@@ -258,24 +238,25 @@ function init() {
 
     //
     projectionMatrix = mat4();
-    //projectionMatrix = ProjectionPerspective(projectionMatrix);
-    gl.uniformMatrix4fv(gl.getUniformLocation(program, "uProjectionMatrix"),  false, flatten(projectionMatrix));
-  
+    
+    
+
 
     document.getElementById("ButtonX").onclick = function () { axis = xAxis; };
     document.getElementById("ButtonY").onclick = function () { axis = yAxis; };
     document.getElementById("ButtonZ").onclick = function () { axis = zAxis; };
     document.getElementById("ButtonT").onclick = function () { flag = !flag; };
+    document.getElementById("ButtonPers").onclick = function () { perspectiveFlag = !perspectiveFlag; };
 
     render();
 }
 
 var render = function () {
 
-    var scaleFactor= 0.2;
-    var cordShiftX= 0;
-    var cordShiftY= 0;
-    var cordShiftZ= 0;
+    var scaleFactor = 0.2;
+    var cordShiftX = 0;
+    var cordShiftY = 0;
+    var cordShiftZ = 0;
 
     var rotateX = 0;
     var rotateY = 0;
@@ -285,42 +266,52 @@ var render = function () {
         0, scaleFactor, 0, 0,
         0, 0, scaleFactor, 0,
         0, 0, 0, 1);
-     var translationMat = mat4(1, 0, 0, cordShiftX,
+    var translationMat = mat4(1, 0, 0, cordShiftX,
         0, 1, 0, cordShiftY,
         0, 0, 1, cordShiftZ,
         0, 0, 0, 1);
 
 
-     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-     if (flag) theta[axis] += 1.0;
+    if (flag) theta[axis] += 1.0;
 
-     modelViewMatrix = mat4();
-     modelViewMatrix = mult(modelViewMatrix, scaleMat);
-      modelViewMatrix = mult(modelViewMatrix, translationMat);
-     modelViewMatrix = mult(modelViewMatrix, rotate(theta[xAxis] + rotateX, vec3(1, 0, 0)));
-     modelViewMatrix = mult(modelViewMatrix, rotate(theta[yAxis] + rotateY, vec3(0, 1, 0)));
-     modelViewMatrix = mult(modelViewMatrix, rotate(theta[zAxis] + rotateZ, vec3(0, 0, 1)));
+    modelViewMatrix = mat4();
+    modelViewMatrix = mult(modelViewMatrix, scaleMat);
+    modelViewMatrix = mult(modelViewMatrix, translationMat);
+    modelViewMatrix = mult(modelViewMatrix, rotate(theta[xAxis] + rotateX, vec3(1, 0, 0)));
+    modelViewMatrix = mult(modelViewMatrix, rotate(theta[yAxis] + rotateY, vec3(0, 1, 0)));
+    modelViewMatrix = mult(modelViewMatrix, rotate(theta[zAxis] + rotateZ, vec3(0, 0, 1)));
+    gl.uniformMatrix4fv(gl.getUniformLocation(program, "uModelViewMatrix"), false, flatten(modelViewMatrix));
 
-     gl.uniformMatrix4fv(gl.getUniformLocation(program,"uModelViewMatrix"), false, flatten(modelViewMatrix));
-     //gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-     // if (flag) theta[axis] += 2.0;
-     //gl.uniform3fv(thetaLoc, theta);
-     gl.drawArrays(gl.TRIANGLES, 0, numPositions);
+    //light
+    lightPosition[0] = 2.5*Math.sin(0.06*time);
+    lightPosition[2] = 2.5*Math.cos(0.06*time);
+    time += 1;
+    gl.uniform4fv(gl.getUniformLocation(program, "uLightPosition"), lightPosition);
+
+    if(perspectiveFlag)
+    projectionMatrix = ProjectionPerspective(projectionMatrix);
+    else
+    projectionMatrix = mat4();
+    gl.uniformMatrix4fv(gl.getUniformLocation(program, "uProjectionMatrix"), false, flatten(projectionMatrix));
+    
+    gl.drawArrays(gl.TRIANGLES, 0, numPositions);
     requestAnimationFrame(render);
 }
 //
 
 var translationMat2 = mat4(1, 0, 0, 0,
-                           0, 1, 0, 0,
-                           0, 0, 1, -1,
-                           0, 0, 0, 1);
+    0, 1, 0, 0,
+    0, 0, 1, -1,
+    0, 0, 0, 1);
+    //used for projection perspective
 function ProjectionPerspective(projectionMatrix) {
     var fovy = 100, aspect = 1, near = 0, far = 100;
     projectionMatrix = perspective(fovy, aspect, near, far);
- 
+
     projectionMatrix = mult(projectionMatrix, translationMat2);
     gl.uniformMatrix4fv(gl.getUniformLocation(program, "uProjectionMatrix"),
-       false, flatten(projectionMatrix));
-       return projectionMatrix;
- }
+        false, flatten(projectionMatrix));
+      return projectionMatrix;
+}
